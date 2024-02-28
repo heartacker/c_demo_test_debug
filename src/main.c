65394d039a38d53273a8e851af7904577e8183ab
Acker Liu <acl042@clourneysemi.com>
2024-02-28 14:29:42 +0800

    table

--- src/main.c
+++ src/main.c
@@ -3,52 +3,52 @@
 
 #include "afe_common.h"
 
-void a();
+void display_sizeof();
 
 int main(int argc, char const *argv[])
 // int main(int argc, const char *argv[])
 {
-#if DEBUG
-    printf("%ld\n", sizeof(char));
-    printf("%ld\n", sizeof(int8_t));
-
-    printf("%ld\n", sizeof(unsigned char));
-    printf("%ld\n", sizeof(uint8_t));
-
-    printf("%ld\n", sizeof(short));
-    printf("%ld\n", sizeof(int16_t));
-
-    printf("%ld\n", sizeof(unsigned short));
-    printf("%ld\n", sizeof(uint16_t));
-
-    printf("%ld\n", sizeof(int));
-    printf("%ld\n", sizeof(int32_t));
-
-    printf("%ld\n", sizeof(unsigned int));
-    printf("%ld\n", sizeof(uint32_t));
-
-    printf("%ld\n", sizeof(unsigned long));
-    printf("%ld\n", sizeof(uint64_t));
-#endif // DEBUG
-
+    display_sizeof();
     for (uint32_t i = 0; i < argc; i++) {
-        printf("%d:\t %s\n", i, argv[i]);
+        printf("%d: %s\n", i, argv[i]);
     }
-
     strcpy(argv[0], "afe_cmd");
-
     for (int32_t i = 0; i < argc; i++) {
         printf("%u: %s\n", i, argv[i]);
     }
-    printf("");
-
-    a();
     return 0;
 }
 
-void a()
+void display_sizeof()
 {
-    printf("");
+#if DEBUG
+    printf("sizeof(char)\t\t\t=\t%ld\n", sizeof(char));
+    printf("sizeof(int8_t)\t\t\t=\t%ld\n", sizeof(int8_t));
+
+    printf("sizeof(unsigned char)\t\t=\t%ld\n", sizeof(unsigned char));
+    printf("sizeof(uint8_t)\t\t\t=\t%ld\n", sizeof(uint8_t));
+
+    printf("sizeof(short)\t\t\t=\t%ld\n", sizeof(short));
+    printf("sizeof(int16_t)\t\t\t=\t%ld\n", sizeof(int16_t));
+
+    printf("sizeof(unsigned short)\t\t=\t%ld\n", sizeof(unsigned short));
+    printf("sizeof(uint16_t)\t\t=\t%ld\n", sizeof(uint16_t));
+
+    printf("sizeof(int)\t\t\t=\t%ld\n", sizeof(int));
+    printf("sizeof(int32_t)\t\t\t=\t%ld\n", sizeof(int32_t));
+
+    printf("sizeof(unsigned int)\t\t=\t%ld\n", sizeof(unsigned int));
+    printf("sizeof(uint32_t)\t\t=\t%ld\n", sizeof(uint32_t));
+
+    printf("sizeof(unsigned long)\t\t=\t%ld\n", sizeof(unsigned long));
+    printf("sizeof(uint64_t)\t\t=\t%ld\n", sizeof(uint64_t));
+
+    printf("sizeof(long long)\t\t=\t%ld\n", sizeof(long long));
+    printf("sizeof(int128_t)\t\t=\t%ld\n", sizeof(int128_t));
+
+    printf("sizeof(unsigned long long)\t=\t%ld\n", sizeof(unsigned long long));
+    printf("sizeof(uint128_t)\t\t=\t%ld\n", sizeof(uint128_t));
+#endif // DEBUG
 }
 
 #if 0

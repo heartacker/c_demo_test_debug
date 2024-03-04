d5ac5ea2e15197064500372c24d03533f0e243a1
Acker Liu <acl042@clourneysemi.com>
2024-03-04 14:09:09 +0800

    __cplusplus

--- src/afe.h
+++ src/afe.h
@@ -1,6 +1,18 @@
 #ifndef __AFE__H__
 #define __AFE__H__
 
+#ifdef __cplusplus
+#if __cplusplus
+extern "C" {
+#endif
+#endif /* __cplusplus */
+
 #include "afe_common.h"
 
-#endif  //!__AFE__H__
+#ifdef __cplusplus
+#if __cplusplus
+}
+#endif
+#endif /* __cplusplus */
+
+#endif //!__AFE__H__
